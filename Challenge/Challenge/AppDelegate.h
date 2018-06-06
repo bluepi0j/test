38a20efcae0967dc532b8fdc6e27226c7e6a4c5b
bluepi0j <vicwang95@gmail.com>
2018-06-05 22:12:34 -0400

    init project

---
+++ Challenge/Challenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Challenge
+//
+//  Created by Vic on 2018-06-05.
+//  Copyright © 2018 bluepi0j. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Challenge/Challenge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Challenge
+//
+//  Created by Vic on 2018-06-05.
+//  Copyright © 2018 bluepi0j. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
